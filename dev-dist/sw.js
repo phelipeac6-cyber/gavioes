1d9a639ae51a47a8fbd54c4076ccdeb0d6845684
[dyad] <phelipeac6@gmail.com>
2025-10-09 11:05:54 -0300

    [dyad] Remoção do botão da Carteirinha de Emergência - wrote 1 file(s) + extra files edited outside of Dyad

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.mabslh7r548"
+    "revision": "0.im1rogabnbg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
